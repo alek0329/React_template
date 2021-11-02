0bd523e722a5725c2fe8f33b82b83b056ea5f1ac
alek0329 <alek0329@gmail.com>
2021-11-02 18:33:12 +0100

    halfway there

---
+++ src/components/counters.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Counter from "./counter";
+
+class Counters extends React.Component {
+    render() { 
+        return (<div></div>);
+    }
+}
+ 
+export default Counters;
