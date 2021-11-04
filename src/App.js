27eb46e3b4df55c4d701c9b116d130dc5d2aa5df
alek0329 <alek0329@gmail.com>
2021-11-04 21:32:02 +0100

    first section done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import upper, { text1, text2, text3 } from "./file1";
+import obj, { males, females } from "./file2";
+
+var person = obj;
+const male = males;
+const female = females;
+const people = male.concat(female);
+
+const { email, firstName, gender, lastName } = person;
+
+const personV2 = {
+  Email: email,
+  Name: firstName,
+  LastName: lastName,
+  Friends: people,
+  Gender: gender,
+  phone: "123456",
+};
 
 function App() {
+  console.log(...male, "kurt", "Helle", ...female, "tina");
+  console.log(personV2);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h2>Ex1</h2>
+      <p>{text1}</p>
+      <p>{text2}</p>
+      <p>{text3}</p>
+      <p>{upper("please uppercase me")}</p>
+      <h2>Ex2</h2>
+      <p>{email}</p>
+      <p>{firstName}</p>
+    </React.Fragment>
   );
 }
 
