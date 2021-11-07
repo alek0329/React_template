a20c37d99652cd478dec0121e83bc8cd71e68e0f
Alek0329 <alek0329@gmail.com>
2021-11-07 18:27:42 +0100

    works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   const initNum = 5;
   return (
     <div>
-      <Counter {initNum} />
+      <button onClick={() => initNum == 5}>Start</button>
+      <Counter incrementor={initNum} />
     </div>
   );
 }
 
 function Counter(props) {
-  console.log(props);
-  const [counter, setCounter] = useState(props.initNum);
+  const [counter, setCounter] = useState(props.incrementor);
+  localStorage.setItem("count", counter);
+  let storedCount = localStorage.getItem("count");
   return (
     <div>
-      <button onClick={() => setCounter(counter + 1)}>Click me!</button>
-      <h3>counter: {counter}</h3>;
+      <button
+        className="btn-sm btn-success m-2"
+        onClick={() => setCounter(counter + props.incrementor)}
+      >
+        +
+      </button>
+      <button
+        className="btn-sm btn-danger m-2"
+        onClick={() => setCounter(counter - props.incrementor)}
+      >
+        -
+      </button>
+      <h3>StoredCounter: {storedCount}</h3>
+      <h3>Counter: {counter}</h3>
     </div>
   );
 }
+
+// function Storager(props) {
+//   localStorage.setItem("count", props.counter);
+//   const storedCount = localStorage.getItem("count");
+//   useEffect(() => {
+//     document.title = `You clicked ${props.counter} times`;
+//   });
+//   return (
+//     <React.Fragment>
+//       <h3>counter: {storedCount}</h3>
+//     </React.Fragment>
+//   );
+// }
 
 export default App;
