cd4b8f51c92cde3a5d3b3b0d0ba8a7771b3a164d
Alek0329 <alek0329@gmail.com>
2021-11-07 15:55:01 +0100

    123

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component, useState } from "react";
-import Counter from "./components/counter";
 import "./App.css";
 
 function App() {
+  const initNum = 1;
   return (
-    <div className="App">
-      <Counter count(3) />
+    <div>
+      <Counter initNum />
+    </div>
+  );
+}
+
+function Counter(props) {
+  const [counter, setCounter] = useState(props.initNum);
+  return (
+    <div>
+      <button onClick={() => setCounter(counter + 1)}>Click me!</button>
+      <h3>counter: {counter}</h3>;
     </div>
   );
 }
