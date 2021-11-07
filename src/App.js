78c7dcc0f6846cb8d5f920dd2f292248debbf89c
Alek0329 <alek0329@gmail.com>
2021-11-07 14:47:18 +0100

    123

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={countIncrement}>Click me!</button>
+      <h3>count: {countIncrement.count}</h3>
     </div>
   );
 }
 
+function countIncrement() {
+  const count = 0;
+  console.log(count);
+}
 export default App;
