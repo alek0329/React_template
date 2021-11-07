edc3c2d0e1223c7ac34f02fde89ae77c51e31a91
Alek0329 <alek0329@gmail.com>
2021-11-07 16:00:46 +0100

    123

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, { Component, useState } from "react";
 import "./App.css";
 
 function App() {
-  const initNum = 1;
+  const initNum = 5;
   return (
     <div>
-      <Counter initNum />
+      <Counter {initNum} />
     </div>
   );
 }
 
 function Counter(props) {
+  console.log(props);
   const [counter, setCounter] = useState(props.initNum);
   return (
     <div>
