c86750ee766f311e1649cfef290f2de3db9f2d25
Alek0329 <alek0329@gmail.com>
2021-11-09 22:43:45 +0100

    done

---
+++ src/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+  return {
+      ClientId: 'ef29c131f9924126a2d69723d0ee265c',
+      ClientSecret: '59ee39aeadbb4b4b8d8805e523ae51fe'
+  }
+}
+
+export { Credentials };
